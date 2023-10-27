2faef046ba3d52675f7289d7e41e5d89b2f08ba6
Diman Randisa <dimanrandisa@gmail.com>
2023-10-27 23:28:39 +0530

    Create Account Changes Done.

--- app/src/main/java/com/example/notesapp/CreateAccountActivity.java
+++ app/src/main/java/com/example/notesapp/CreateAccountActivity.java
@@ -3,12 +3,62 @@ package com.example.notesapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class CreateAccountActivity extends AppCompatActivity {
+
+    EditText emailEditText,passwordEditText,confirmPasswordEditText;
+    Button createAccountBtn;
+    ProgressBar progressBar;
+    TextView loginBtnTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        emailEditText = findViewById(R.id.email_edit_text);
+        passwordEditText = findViewById(R.id.password_edit_text);
+        confirmPasswordEditText = findViewById(R.id.confirm_password_edit_text);
+        createAccountBtn = findViewById(R.id.create_account_btn);
+        progressBar = findViewById(R.id.progress_bar);
+        loginBtnTextView = findViewById(R.id.login_text_view_btn);
+
+        createAccountBtn.setOnClickListener(v-> createAccount());
+        loginBtnTextView.setOnClickListener(v-> finish());
+
     }
+
+    private void createAccount() {
+        String email  = emailEditText.getText().toString();
+        String password  = passwordEditText.getText().toString();
+        String confirmPassword  = confirmPasswordEditText.getText().toString();
+
+
+
+    }
+
+    boolean validateData(String email,String password,String confirmPassword){
+        //validate the data that are input by user.
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailEditText.setError("Email is invalid");
+            return false;
+        }
+        if(password.length()<6){
+            passwordEditText.setError("Password length is invalid");
+            return false;
+        }
+        if(!password.equals(confirmPassword)){
+            confirmPasswordEditText.setError("Password not matched");
+            return false;
+        }
+        return true;
+    }
+
+
 }
